cbff233f58fab398db92a41f1c5e3243da1a259d
Роман Кондрашев <distanceon@MacBook-Air-Roman.local>
2024-01-09 02:06:25 +0300

    fix: fix init project to correct boiler

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './style/index.less'
+
+import { AppThemeProvider } from './styles/ThemeProvider.tsx'
+import { App } from './components/App/App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <AppThemeProvider>
+      <App />
+    </AppThemeProvider>
+  </React.StrictMode>,
 )
--- src/utils/utils.ts
+++
@@ -1,4 +0,0 @@
-import tailwindConfig from 'tailwind.config.js'; // IMPORTANT that the path is NOT relative, only the file name
-import resolveConfig from 'tailwindcss/resolveConfig';
-
-export default resolveConfig(tailwindConfig as any)
